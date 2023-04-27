3630814ebb34b474824fab28e1a706711a666dc1
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-27 19:12:21 +0300

    add win cards render with excel data

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+import pandas
 
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+def years_ru(years_gap):
+    last_digit = years_gap % 10
+    if last_digit == 0 or (5 <= last_digit <= 9) or (11 <= years_gap % 100 <= 14):
+        return 'лет'
+    if 2 <= last_digit <= 4:
+        return 'года'
+    return 'год'
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+years_gap = datetime.datetime.now().year - 1920
+
+wines = pandas.read_excel('wine.xlsx')
+wines.columns = ['name', 'kind', 'price', 'image']
+
+rendered_page = template.render(
+    wines=wines.to_dict('records'),
+    age=f'{str(years_gap)} {years_ru(years_gap)}',
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
